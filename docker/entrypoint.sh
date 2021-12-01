402d7dcade46a89e9a189125fd59c8c97dfa34d0
Arved Enders-Seidlitz <arved.enders-seidlitz@ikz-berlin.de>
2021-12-01 09:16:13 +0100

    dirty solution: use docker as root. Cleanup.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-USER_ID=${LOCAL_UID:-9001}
-GROUP_ID=${LOCAL_GID:-9001}
+# USER_ID=${LOCAL_UID:-9001}
+# GROUP_ID=${LOCAL_GID:-9001}
 
-echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
-useradd -u $USER_ID -o -m user
-groupmod -g $GROUP_ID user
-export HOME=/home/user
+# echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
+# useradd -u $USER_ID -o -m user
+# groupmod -g $GROUP_ID user
+# export HOME=/home/user
 
 source /usr/lib/openfoam/openfoam2106/etc/bashrc
 
@@ -14,4 +14,5 @@ cd ../fenicsx-adapter
 pip3 install -e .
 cd ../workdir
 
-exec /usr/sbin/gosu user "$@"
+exec "$@"
+# exec /usr/sbin/gosu user "$@"
